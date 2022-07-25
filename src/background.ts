a5805600d1403502435fdf2ef3a4a7dc8af03ac9
liangcl <liangcl@pingpongx.com>
2022-07-25 19:22:50 +0800

    feat: 重构代码 适用api

--- src/background.ts
+++ src/background.ts
@@ -1,75 +1,73 @@
-import { useStorage } from "@plasmohq/storage"
 import darkImage from "data-base64:~assets/dark.png"
 import lightImage from "data-base64:~assets/light.png"
+
+import { Storage } from "@plasmohq/storage"
+
 // export {}
 console.log("HELLO WORLD FROM BGSCRIPTS")
 
-class StartServer{
-  localStorageData:string = 'light';
-
-  constructor(){
+class StartServer {
+  localStorageData: string = "light"
+  storage: any = new Storage({ area: "local" })
+  constructor() {
     this.init()
   }
-  init(){
-    chrome.runtime.onInstalled.addListener( ()=> {
-      chrome.storage.local.get(['theme'],  (res) => {
-        if (res.theme) {
-          this.localStorageData = res.theme;
-        } else {
-          this.setThemeMode('light');
-          this.localStorageData = 'light';
-        }
-        this.setIcon()
-        this.listenIconClick()
-      });
-    });
-    chrome.runtime.onMessage.addListener((req,sender, sendResponse) => {
-      sendResponse({theme: this.localStorageData})
+  init() {
+    chrome.runtime.onInstalled.addListener(async () => {
+      const data = await this.getData("theme")
+      if (data) {
+        this.localStorageData = data
+      } else {
+        await this.setThemeMode("light")
+        this.localStorageData = "light"
+      }
+      this.setIcon()
+      this.listenIconClick()
+    })
+    chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+      sendResponse({ theme: this.localStorageData })
     })
   }
+  async getData(key) {
+    return await this.storage.get(key)
+  }
   listenIconClick() {
-    console.log('逻辑中 chrome.action', chrome.action);
-    chrome.action.onClicked.addListener( ()=> {
-      console.log('逻辑中 localStorageData', this.localStorageData);
-      this.localStorageData = this.localStorageData == 'light' ? 'dark' : 'light';
-      this.setThemeMode(this.localStorageData);
+    chrome.action.onClicked.addListener(async () => {
+      this.localStorageData =
+        this.localStorageData == "light" ? "dark" : "light"
+      await this.setThemeMode(this.localStorageData)
       this.setIcon()
       this.changeTabTheme()
-    });
+    })
   }
-  private setIcon(){
-    const path = this.localStorageData == 'light' ? lightImage: darkImage
-    chrome.action.setIcon({ path });
+  private setIcon() {
+    const path = this.localStorageData == "light" ? lightImage : darkImage
+    chrome.action.setIcon({ path })
   }
-  changeTabTheme(){
-    chrome.tabs.query({},
-      (tabs) => {
-        for (var i = 0; i < tabs.length; i++) {
-          console.log('获取url', tabs[i].url);
-          try {
-            const location = new URL(tabs[i].url)
-            const host = location.host
-            if (host.includes('juejin.cn')) {
-              let message = {
-                theme: this.localStorageData,
-              };
-              chrome.tabs.sendMessage(tabs[i].id, message, (res) => {
-                console.log('background=>content');
-                console.log(res);
-              });
+  changeTabTheme() {
+    chrome.tabs.query({}, (tabs) => {
+      for (var i = 0; i < tabs.length; i++) {
+        console.log("获取url", tabs[i].url)
+        try {
+          const location = new URL(tabs[i].url)
+          const host = location.host
+          if (host.includes("juejin.cn")) {
+            let message = {
+              theme: this.localStorageData
             }
+            chrome.tabs.sendMessage(tabs[i].id, message, (res) => {
+              console.log("background=>content")
+              console.log(res)
+            })
           }
-          catch (e) {
-            console.log('报错',e);
-          }
+        } catch (e) {
+          console.log("报错", e)
         }
       }
-    );
+    })
   }
-  setThemeMode(mode) {
-    chrome.storage.local.set({ theme: mode }, function(){
-      console.log('设置了theme', mode);
-    });
+  async setThemeMode(mode) {
+    await this.storage.set("theme", mode)
   }
 }
 new StartServer()
